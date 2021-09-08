3aa750d378757baeba4b0693418dcca4138ee615
satyam sharma <satyamyashsatyamyash@gmail.com>
2021-09-08 22:27:11 +0530

    First Master Commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <Link to="/">ShakesJeer</Link>
+        <Link to="/About">About</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;

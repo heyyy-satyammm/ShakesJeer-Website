b515886218ab69e42743480e94ad624d5b559644
Unnat2504 <63155197+Unnat2504@users.noreply.github.com>
2021-09-24 23:27:57 +0530

    nav

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
+// import React from "react";
+// import "./App.css";
+// import { BrowserRouter, Route, Switch } from "react-router-dom";
+// import Home from "./components/Home/Home";
+// import About from "./components/About/About";
+// import Navbar from "./components/Navbar/Navbar";
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <div className="App">
+//         <Navbar />
+//         <Switch>
+//           <Route exact path="/" component={Home} />
+//           <Route path="/about" component={About} />
+//         </Switch>
+//       </div>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from "./components/Home/Home";
-import About from "./components/About/About";
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+//import Reports from "./pages/Reports";
+import Products from "./pages/Products";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
+    <>
+      <Router>
         <Navbar />
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
+          <Route path="/" exact component={Home} />
+          {/* <Route path="/reports" component={Reports} /> */}
+          <Route path="/about" component={Products} />
         </Switch>
-      </div>
-    </BrowserRouter>
+      </Router>
+    </>
   );
 }
 

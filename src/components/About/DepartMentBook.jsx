7e4bdb70538d06a59f898bf75c358c070348577d
Raghvendra <rv.singh.5330@gmail.com>
2021-09-24 18:11:27 +0530

    department image added, bubble effect added

--- src/components/About/DepartMentBook.jsx
+++ src/components/About/DepartMentBook.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import logo from '../../Assest/logo.png'
 
 function DepartMentBook(props) {
     return (<div className="card " data-aos="fade-up" data-aos-offset="200" data-aos-duration="1000"
         style={
             { height: "55vh" }} >
         <div className="imgBox" >
-            <div>
-                <p style={{fontWeight:"600", fontSize:"28px", textAlign:"center"}}>{props.department} </p>
-                <p style={{paddingLeft:"40px", paddingRight:"40px"}}>{props.description}</p>
+            <div style={{overflow:"hidden",borderRadius:"15px"}}>
+                <p style={{ fontWeight: "600", fontSize: "28px", textAlign: "center" }}>{props.department} </p>
+                <div style={{position:"relative", display:"grid", gridTemplateColumns:"50% 50%",}}>
+                    <p style={{ paddingLeft: "40px", paddingRight: "40px",}}>{props.description}</p>
+                    <div style={{backgroundImage:`url(${logo})`, backgroundSize:"cover",position:"relative",}}></div>
+                </div>
             </div>
             <div>
                 <img src="unnat sir2.jpg" alt="" />

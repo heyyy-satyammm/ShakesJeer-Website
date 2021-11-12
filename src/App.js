db1a52e86807ac6ba2bd4f931bf7daa02895a259
Raghvendra <rv.singh.5330@gmail.com>
2021-11-12 21:53:31 +0530

    Event page first commit : RV

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React,{useEffect} from "react";
-import AOS from 'aos';
-import 'aos/dist/aos.css'
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
+import Event from "./components/Event/Event";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  useEffect(()=>{
+  useEffect(() => {
     AOS.init();
     AOS.refresh();
-  })
+  });
   return (
     <BrowserRouter>
       <div className="App">
@@ -19,6 +20,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
+          <Route path="/event" component={Event} />
         </Switch>
       </div>
     </BrowserRouter>

f074fc85e72b3b18725c1e45643c352e9c5e5882
Unnat2504 <63155197+Unnat2504@users.noreply.github.com>
2021-11-28 17:03:51 +0530

    finall

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,6 +6,8 @@ import { SidebarData } from "./SidebarData";
 import "./Navbar.css";
 import { IconContext } from "react-icons";
 import logo from "./logo.png";
+import lightblue_darkbg from "./lightblue_darkbg.png";
+import yellowlogo_lightbg from "./yellowlogo_lightbg.png";
 
 function Navbar() {
   const [sidebar, setSidebar] = useState(false);
@@ -14,7 +16,7 @@ function Navbar() {
 
   return (
     <>
-      <IconContext.Provider value={{ color: "#fec701" }} >
+      <IconContext.Provider value={{ color: "#fec701" }}>
         <div className="full">
           <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
             <ul className="nav-menu-items" onClick={showSidebar}>
@@ -26,8 +28,8 @@ function Navbar() {
               {SidebarData.map((item, index) => {
                 return (
                   <li key={index} className={item.cName}>
-                    <Link to={item.path} >
-                      {item.icon }
+                    <Link to={item.path}>
+                      {item.icon}
                       <span>{item.title}</span>
                     </Link>
                   </li>
@@ -37,7 +39,12 @@ function Navbar() {
           </nav>
           <div className="navbar">
             <div className="logo">
-            <img src={logo} alt="logo" width="70px" />
+              <img
+                src={yellowlogo_lightbg}
+                alt="logo"
+                width="110px"
+                height="75px"
+              />
             </div>
             <Link to="#" className="menu-bars">
               <FaIcons.FaBars onClick={showSidebar} />
